6b2ece0daa7baa11a5f9ea064dacb87eeba036e7
Chad Smith <smithchad17@gmail.com>
2024-03-06 12:37:10 -0500

    Initial setup

---
+++ BackgroundWorkerService.cs
@@ -0,0 +1,29 @@
+﻿
+public class BackgroundWorkerService : IHostedService
+{
+    readonly ILogger<BackgroundWorkerService> _logger;
+
+    public BackgroundWorkerService(ILogger<BackgroundWorkerService> logger)
+    {
+            _logger = logger;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Service started.");
+
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            Task.Delay(1000, cancellationToken).Wait();
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Service stopped.");
+        return Task.CompletedTask;
+    }
+}
